8a0424a6cffd7289c109ee467366e8ea36482344
Saba <sabavaridze90@gmail.com>
2021-12-08 19:02:24 +0400

    vizuali

---
+++ script.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.multiple-items').slick({
+     infinite: true,
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     arrows: true,
+     responsive: [
+         {
+           breakpoint: 1417,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1
+           }
+         },
+         {
+           breakpoint: 480,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
+     ]
+   });    
+ });
